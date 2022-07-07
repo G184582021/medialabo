f7e3aa2e3e2df4f28eb42c12ba1394f99523f317
G184582021 <218458@st.takushoku-u.ac.jp>
2022-07-07 15:34:46 +0900

    a

--- kazuate.js
+++ kazuate.js
@@ -5,40 +5,35 @@ console.log('答え: ' + kotae);      // デバッグ用
 // 入力回数（予想回数）
 let kaisu = 0;
 
-// 予想を4回実行する
-// 将来: ボタンを押したら， hantei() を呼び出すように修正する
-hantei();
-hantei();
-hantei();
-hantei();
+let b = document.querySelector('#print');
+    b.addEventListener('click', hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
-    let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
-    // 課題3-1：ここの判定処理を作成する．
-    //        ページに表示する方法はまだ習っていないので
-    //        判定結果はコンソールに出力すること
-    console.log(kaisu + '回目の予想: ' + yoso);
-
+function hantei() { 
+    kaisu = kaisu + 1;      
     
-    let d = document.querySelector('p#result');
+    let i = document.querySelector('input[name="seisuu"]');
+    let l = i.value - 0;
+        
+    let k = document.querySelector('span#answer');
+    k.textContent = kaisu + '回目の予想: ';
+
+    let y = document.querySelector('span#answer1');
+    y.textContent = l;
+
+    let s = document.querySelector('p#result');
 
     if (kaisu >= 4) {
-        let a = "答えは" + kotae + "でした。すでにゲームは終わっています。";
-        d.insertAdjacentElement('beforeend', a);
+        s.textContent = "答えは" + kotae + "でした。すでにゲームは終わっています。";
     } else if (kotae === yoso) {
-        let a = '正解です。おめでとう!';
-        d.insertAdjacentElement('beforeend', a);
+        s.textContent = '正解です。おめでとう!';
     } else if (kotae !== yoso) {  
         if (kaisu === 3) {
-            let a = 'まちがい。残念でした答えは' + kotae +  'です．';
-            d.insertAdjacentElement('beforeend', a);
+            s.textContent = 'まちがい。残念でした答えは' + kotae +  'です．';
         } else if (kotae > yoso) {
-            let a = 'まちがい。答えはもっと大きいですよ';
-            d.insertAdjacentElement('beforeend', a);
+            s.textContent = 'まちがい。答えはもっと大きいですよ';
         } else if (kotae < yoso) {
-            let a = 'まちがい。答えはもっと小さいですよ';
-            d.insertAdjacentElement('beforeend', a);;
+            s.textContent = 'まちがい。答えはもっと小さいですよ';
         }
     }
 }
